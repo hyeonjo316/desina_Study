3a2cb82039a92551ad0f99c3284e39a64f1c5eda
hyeonjo316 <ekwndjv75@gamil.com>
2026-03-19 17:54:18 +0900

    PlayerMove

---
+++ desina123/Assets/Scripts/CameraFollw.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class CameraFollw : MonoBehaviour
+{
+    public Transform target;
+    public Vector3 offset = new Vector3(0f, 12f, -8f);
+    
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        if (target == null) return;
+
+        transform.position = target.position + offset;
+
+    }
+}
